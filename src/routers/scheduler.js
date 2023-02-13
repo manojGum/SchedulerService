2330ba89b3d4b1118fcd5c0b8140bafd528178bf
manoj kumar <Manojgum@gmail.com>
2023-02-13 17:06:57 +0530

    Update scheduler.js

--- src/routers/scheduler.js
+++ src/routers/scheduler.js
@@ -16,25 +16,3 @@ router.post("/updateScheduler", scheduler.updateScheduler);
 router.post("/stopScheduler", scheduler.stopScheduler);
 module.exports = router;
 // https://crontab.cronhub.io/
-
-/* for developer refrence its temprory for some times */
-
-// // CodeNEntertainment
-// DevTainment
-// The Coding Entertainer
-// EntertainTech
-// WebWit
-// Coding & Comedy
-// TechTalks & Fun
-// DevLaugh
-// The Entertaining Developer
-// CodeClowns.
-// */2 * * * * cron schedule every 2 minute
-// 0 */2 * * *  cron schedule every 2 hours
-// 0 6 * * * cron schedule every day 6 am
-// 0 17 * * 6 This crontab entry runs the script at 17:00 (5PM) on Saturdays:
-// 0 0 1 * * every months At 12:00am on the 1st
-// // https://linuxize.com/post/scheduling-cron-jobs-with-crontab/#:~:text=There%20are%20several%20special%20Cron%20schedule%20macros%20used,midnight%20%2812%3A00%20am%29%20of%20the%201st%20of%20January.
-// Run the a script at 9:15pm, on the 1st and 15th of every month:
-
-// 15 9 1,15 * * /path/to/script.sh
